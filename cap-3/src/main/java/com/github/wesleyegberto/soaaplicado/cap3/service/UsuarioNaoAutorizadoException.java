b6e95b868f5e7e14059641f33149c1e74a3f2414
Wesley Egberto <wesleyegberto@yahoo.com.br>
2015-06-07 21:09:35 -0300

    Cap 3

--- cap-3/src/main/java/com/github/wesleyegberto/soaaplicado/cap3/service/UsuarioNaoAutorizadoException.java
+++ cap-3/src/main/java/com/github/wesleyegberto/soaaplicado/cap3/service/UsuarioNaoAutorizadoException.java
@@ -1,5 +1,8 @@
 package com.github.wesleyegberto.soaaplicado.cap3.service;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.ws.WebFault;
 
 @WebFault(targetNamespace = "http://service.cap3.soaaplicado.wesleyegberto.github.com/exceptions", name = "UsuarioNaoAutorizado")
@@ -25,5 +28,28 @@ public class UsuarioNaoAutorizadoException extends Exception {
 	public UsuarioNaoAutorizadoException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public UsuarioFaultInfo getFaultInfo() {
+		return new UsuarioFaultInfo(getMessage());
+	}
+
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public static class UsuarioFaultInfo {
+		@XmlAttribute
+		private String mensagem;
+
+		public UsuarioFaultInfo(String mensagem) {
+			this.mensagem = mensagem;
+		}
+
+		public String getMensagem() {
+			return mensagem;
+		}
+
+		public void setMensagem(String mensagem) {
+			this.mensagem = mensagem;
+		}
+
 	}
 }
